b789965c20bbad9083653900979007a80acc8a92
Terry Trent <terry.trent@gmail.com>
2016-08-05 14:40:16 -0400

    initial commit

---
+++ WPF_ADAuth_CSharp/WPF_ADAuth_CSharp/ADAuthentication.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.DirectoryServices.Protocols;
+using System.DirectoryServices.ActiveDirectory;
+using DnDns.Enums;
+using DnDns.Query;
+
+namespace WPF_ADAuth_CSharp
+{
+    public class ADAuthentication
+    {
+        public static List<string> authorizedUsers = new List<string>();
+        public static Dictionary<string, string> ldapErrors = new Dictionary<string, string>();
+
+        public ADAuthentication()
+        {
+            setAuthorizedUsers();
+            setLDAPErrors();
+        }
+
+        private void setLDAPErrors()
+        {
+            ldapErrors.Add("52e", "Username or Password incorrect");
+            ldapErrors.Add("530", "User Account not permitted to logon during this time");
+            ldapErrors.Add("531", "User Account not permitted to logon at this computer");
+            ldapErrors.Add("532", "User Account password expired");
+            ldapErrors.Add("533", "User Account disabled");
+            ldapErrors.Add("701", "User Account expired");
+            ldapErrors.Add("773", "User Account password must be reset");
+            ldapErrors.Add("775", "User Account Locked");
+        }
+
+        private void setAuthorizedUsers()
+        {
+            authorizedUsers.Add("zzzttrent");
+            authorizedUsers.Add("ttrent"); 
+        }
+
+        public static void bindToLDAPServer(string ldapServer, string username, string password)
+        {
+            LdapConnection connection = new LdapConnection(ldapServer);
+            NetworkCredential credential = new NetworkCredential(username, password);
+            connection.Credential = credential;
+            connection.Bind();
+        }
+
+        public static string getLdapServer()
+        {
+            Domain localDomain = Domain.GetCurrentDomain();
+            string localDomainAsString = localDomain.ToString();
+
+            DnsQueryRequest request = new DnsQueryRequest();
+            DnsQueryResponse Response = request.Resolve($"_ldap.{localDomainAsString}", NsType.A, NsClass.ANY, System.Net.Sockets.ProtocolType.Udp);
+
+            string answer = Response.AuthoritiveNameServers[0].Answer;
+
+            int indexOfDomain = answer.IndexOf($".{localDomainAsString}");
+            int lengthToSkip = 20;
+            int lengthOfLdapServer = indexOfDomain - lengthToSkip;
+            string nameOfLdapServer = $"{answer.Substring(lengthToSkip, lengthOfLdapServer)}.{localDomainAsString}";
+            return nameOfLdapServer;
+        }
+
+        public static string returnErrorMessage(LdapException lexc)
+        {
+            string error = lexc.ServerErrorMessage;
+            string errorcode = error.Substring(77, 3);
+            string errorMessage = ADAuthentication.ldapErrors[errorcode];
+
+            return errorMessage;
+        }
+    }
+}
---
+++ WPF_ADAuth_CSharp/WPF_ADAuth_CSharp/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.Windows;
+using System.DirectoryServices.Protocols;
+
+namespace WPF_ADAuth_CSharp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ADAuthentication adauth = new ADAuthentication();
+        }
+
+        private void btn_Submit_Click(object sender, RoutedEventArgs e)
+        {
+            bool successfulLogin = AuthenticateUser();
+            if(!successfulLogin)
+            {
+                btn_Submit.Content = "Try Again";
+            }
+        }
+
+        private bool AuthenticateUser()
+        {
+            string username = tb_Username.Text;
+            string password = pb_Password.Password;
+
+            if(username == ""  | password == "")
+            {
+                MessageBox.Show("You must enter both a username and a password");
+                return false;
+            }
+            else
+            { 
+                string ldapServer = ADAuthentication.getLdapServer();
+
+                if (ADAuthentication.authorizedUsers.Contains(username))
+                {
+                    try
+                    {
+                        ADAuthentication.bindToLDAPServer(ldapServer, username, password);
+                        MessageBox.Show("Login Successful");
+                        return true;
+                    }
+                    catch (LdapException lexc)
+                    {
+                        string errorMessage = ADAuthentication.returnErrorMessage(lexc);
+                        MessageBox.Show(errorMessage);
+                        return false;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("User is not authorized");
+                    return false;
+                }
+            }
+        }
+    }
+}
